4609dd7f0d35222a930d0c82e56c705954b0fe39
Jason Li <haotian-li@outlook.com>
2022-01-01 23:03:14 -0800

    update .gitmodules

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,9 @@
+import express from "express";
+
 export const serve = (port: number, filename: string, dir: string) => {
-  console.log("Server is listening on port", port);
-  console.log("saving/fetching cells from", filename);
-  console.log("file is in dir", dir);
+  const app = express();
+
+  app.listen(port, () => {
+    console.log("Listening on port " + port);
+  })
 };
